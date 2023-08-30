12fb358dd63c0e8f887b776b4493602baa819b2c
hamzabelakri <hamzabelakri92@gmail.com>
2023-08-30 15:49:39 +0100

    new url

--- services/index.tsx
+++ services/index.tsx
@@ -1,7 +1,6 @@
 import request, { gql } from "graphql-request";
 
-const URL =process.env.NEXT_PUBLIC_MASTER_URL;
-console.log(URL)
+const URL =process.env.NEXT_PUBLIC_MASTER_URL
 export const getCarsList = async () => {
   const query = gql`
     query CarLists {
